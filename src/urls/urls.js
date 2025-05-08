35b1cc4a8593351250cde9796ec3fbed9c5ec1f3
muhammadabdullahshakir <muhammad.abdullah21000@gmail.com>
2025-05-08 16:48:58 +0500

    vite.config Chnaged

--- src/urls/urls.js
+++ src/urls/urls.js
@@ -4,7 +4,7 @@ import { getUserIdFromLocalStorage } from '../data/localStorage';
 const selectedGatewayId = localStorage.getItem("selectedGatewayId");
 
 
-const API_Base_url = import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000" ;
+const API_Base_url = import.meta.env.VITE_API_BASE_URL || "https://ems-server-530056698.us-central1.run.app" ;
 
 //server url "https://ems-server-530056698.us-central1.run.app"
 
